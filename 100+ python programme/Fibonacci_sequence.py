346ff559fa304a468ad6b56cf81afc1cfa25bf59
Shubham Yadav <yadav.10shubham@gmail.com>
2023-09-03 00:05:43 +0530

    update

--- 100+ python programme/Fibonacci_sequence.py
+++ 100+ python programme/Fibonacci_sequence.py
@@ -1,14 +1,16 @@
 #printing fibonacci sequence
 
-num = int(input("Enter the number : "))
-fact = 1
+a = 0
+b = 1
+num = int(input("Enter a number to obtain fibonacci : "))
 
-if num < 0:
-    print("Factorial of 0 does not exist")
-if num == 0:
-    print(f"Factorial of 0 is {1}")
-
-if num > 0:
-    for i in range(1,num+1):
-        fact = fact * i
-print(f'The factorial of given number is {fact}')
+if num == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2,num):
+        c = a + b
+        a = b
+        b = c
+        print(c)
