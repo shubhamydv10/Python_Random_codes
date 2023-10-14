cec39bdbdeb4351c7b415c77df0527903c8edca4
Shubham Yadav <yadav.10shubham@gmail.com>
2023-10-14 16:03:46 +0530

    update

---
+++ 100+ python programme/shuffle deck of cards.py
@@ -0,0 +1,12 @@
+#To shuffle deck of cards
+
+import random,itertools
+
+deck = list(itertools.product(range(1,14),["Spade","Club","Hearts","Diamond"]))
+
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    print(deck[i][0],"of" ,deck[i][1])
+
